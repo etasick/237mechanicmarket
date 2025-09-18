bf1a3c64af4c0dc8da6698c66e8b50cfdb7a421d
etasick <etasick@gmail.com>
2025-09-18 22:00:46 +0100

    added a fix to the homepage

--- src/amplifyClient.js
+++ src/amplifyClient.js
@@ -5,11 +5,13 @@ import awsconfig from "./aws-exports";
 // Configure Amplify with multi-auth
 Amplify.configure({
   ...awsconfig,
+  aws_appsync_authenticationType: "API_KEY", 
+  ssr:true,
 });
 
 // Default client (Cognito or whatever is in aws-exports)
-export const authClient = generateClient({authMode:"userPool"});
+//export const authClient = generateClient({authMode:"userPool"});
 
 // Public client (API key)
-export const publicClient = generateClient({ authMode: "API_KEY" });
+//export const publicClient = generateClient({ authMode: "API_KEY" });
 
